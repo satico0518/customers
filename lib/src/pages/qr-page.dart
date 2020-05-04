e0a0ba43f8b1516957627bdebfc16de47e791029
DavoDev <davo.gomez1@gmail.com>
2020-05-04 10:42:50 -0500

    qrcode ok

--- lib/src/pages/qr-page.dart
+++ lib/src/pages/qr-page.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-class QRCode extends StatefulWidget {
-
+class QRCodePage extends StatefulWidget {
+  static final String routeName = 'qr';
   @override
-  _QRCodeState createState() => _QRCodeState();
+  _QRCodePageState createState() => _QRCodePageState();
 }
 
-class _QRCodeState extends State<QRCode> {
+class _QRCodePageState extends State<QRCodePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: Text('qr'),
+    final args = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tu Código QR'),
+      ),
+      body: Container(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            children: <Widget>[
+              Text(
+                'Presenta este codigo QR en los establecimientos que visites, de esta manera se puedra cargar toda la información de tu entrevista COVID-19!',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.justify,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                  color: Colors.grey[50],
+                  // child: Image.memory(image)
+                  child: QrImage(
+                    foregroundColor: Colors.purple[900],
+                    data: args.toString(),
+                    version: QrVersions.auto,
+                    size: MediaQuery.of(context).size.width * .9,
+                    errorStateBuilder: (cxt, err) {
+                      return Container(
+                        child: Center(
+                          child: Text(
+                            "Oops! ${err.toString()}",
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      );
+                    },
+                  )),
+            ],
+          )),
     );
   }
 }
---
+++ lib/src/providers/form.provider.dart
@@ -0,0 +1,72 @@
+import 'dart:io';
+
+import 'package:customers/src/models/form.model.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DBProvider {
+  static Database _database;
+
+  static final DBProvider db = DBProvider._();
+
+  DBProvider._();
+
+  Future<Database> get database async {
+    if (_database != null) return _database;
+    _database = await initDB();
+    return _database;
+  }
+
+  initDB() async {
+    Directory documentsdirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsdirectory.path, 'FormDB.db');
+    return await openDatabase(path, version: 1, onOpen: (db) {},
+        onCreate: (Database db, int version) async {
+      await db.execute('CREATE TABLE Form ('
+          'yourSymptoms INTEGER,'
+          'yourHomeSymptoms INTEGER,'
+          'haveBeenIsolated INTEGER,'
+          'haveBeenVisited INTEGER,'
+          'haveBeenWithPeople INTEGER,'
+          'yourSymptomsDesc TEXT,'
+          'haveBeenIsolatedDesc TEXT,'
+          'haveBeenVisitedDesc TEXT,'
+          'isEmployee INTEGER,'
+          'visitorAccept INTEGER,'
+          'employeeAcceptYourSymptoms INTEGER,'
+          'employeeAcceptHomeSymptoms INTEGER,'
+          'employeeAcceptVacationSymptoms INTEGER,'
+          'signature TEXT');
+    });
+  }
+
+  Future<int> addForm(FormModel form) async {
+    final db = await database;
+    final res = await db.insert('Form', form.toJson());
+    return res;
+  }
+
+  Future<List<FormModel>> getForm() async {
+    final db = await database;
+    final form = await db.query('Form');
+    List<FormModel> scanList = form.isNotEmpty
+        ? form.map((x) => FormModel.fromJson(x)).toList()
+        : [];
+    return scanList;
+  }
+
+  // UPDATE
+  updateForm(FormModel form) async {
+    final db = await database;
+    final res = await db
+        .update('Form', form.toJson());
+    return res;
+  }
+
+  Future<int> deleteForm() async {
+    final db = await database;
+    final res = db.delete('Form');
+    return res;
+  }
+}

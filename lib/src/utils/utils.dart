7235a5c915a8dcfbb4c4a85d85dbacf910b82dea
DavoDev <davo.gomez1@gmail.com>
2020-05-14 20:26:41 -0500

    email ok

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 Future<String> _localPath() async {
   final directory = await getApplicationDocumentsDirectory();
@@ -17,4 +19,41 @@ Future<File> writeFileContent(String content) async {
 
   // Write the file
   return file.writeAsString(content);
+}
+
+Future<String> sendEmail(String fileUrl) async {
+  String platformResponse;
+  final Email email = Email(
+    body: '''<h2>Ruta del archivo:</h2> <br/>
+      $fileUrl <br/>
+      <h3>Recuerde que el archivo estara disponible por 2 dias</h3>.
+    ''',
+    subject: 'PaseYa - Link Descarga de Archivo CSV',
+    recipients: ['davo.gomez1@gmail.com'],
+    isHTML: true,
+  );
+
+  try {
+    await FlutterEmailSender.send(email);
+    platformResponse = 'Email Enviado exitosamente!';
+  } catch (error) {
+    platformResponse = error.toString();
+  }
+  return platformResponse;
+}
+
+Future<String> uploadFile(File file, String fileName) async {
+  try {
+    final StorageReference fbStorageRef =
+        FirebaseStorage.instance.ref().child(fileName);
+    StorageUploadTask uploadTask = fbStorageRef.putFile(file);
+    StorageTaskSnapshot snapshot = await uploadTask.onComplete;
+
+    var downloadUrl = await snapshot.ref.getDownloadURL();
+    if (uploadTask.isComplete) return downloadUrl.toString();
+
+    return null;
+  } catch (e) {
+    return null;
+  }
 }
